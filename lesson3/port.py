e55afd68846494d8846f69288df859bc4f771ca8
Leif Kemp <leif.kemp2@bjss.com>
2024-11-11 12:06:39 +0000

    import data files, lesson 3

---
+++ lesson3/port.py
@@ -0,0 +1,20 @@
+# f = open('data/portfolio.csv', 'r')
+# for line in f:
+#     print(line)
+
+total = 0.0
+
+# using with open automatically closes the file when finished
+with open('../data/portfolio.csv', 'r') as f:
+    headers = next(f) # Skips a line
+    for line in f:
+        line = line.strip()
+        parts = line.split(',')
+        parts[0] = parts[0].strip('"')
+        parts[1] = parts[1].strip('"')
+        parts[2] = int(parts[2])
+        parts[3] = float(parts[3])
+        print(parts)
+        total += parts[2]+parts[3]
+
+print('Total cost: %.2f' % total)
---
+++ lesson3/port_csv.py
@@ -0,0 +1,14 @@
+import csv
+
+total = 0.0
+
+with open('../data/portfolio.csv', 'r') as f:
+    rows = csv.reader(f)
+    headers = next(rows) # Skip the header row
+    for row in rows:
+        print(row)
+        row[2] = int(row[2])
+        row[3] = float(row[3])
+        total += row[2]+row[3]
+
+print('Total cost: %.2f' % total)
